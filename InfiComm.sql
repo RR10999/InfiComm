f2a4f3fac77551147343104a1e3267743b8a106a
RR10999 <radheyarajan1705@gmail.com>
2025-02-22 15:42:55 +0530

    Update InfiComm.sql

--- InfiComm.sql
+++ InfiComm.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE InfiComm;
 USE InfiComm;
-
 -- 1. User Table
 CREATE TABLE user (user_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100) NOT NULL, email VARCHAR(100) UNIQUE NOT NULL, contact VARCHAR (15), address TEXT, date_registered DATE);
 -- 2. Customer Table
